dec50f907a71318778e35ed3bb28377659826f4f
Zhu Yi <zhuyi@fusionqb.com>
2025-09-27 10:48:25 +0800

    Add project files.

---
+++ Innojam-SmartCity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Innojam_SmartCity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
